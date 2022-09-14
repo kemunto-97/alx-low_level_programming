7cc5411c1c21a03aa0057fc0fd451703e7cbe4c1
kemunto_97 <kemuntonyamweya@gmail.com>
2022-09-14 06:16:19 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print alphabet in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+char lower;
+lower = 'a';
+while (lower <= 'z')
+{
+_putchar(lower);
+lower++;
+}
+_putchar('\n');
+
