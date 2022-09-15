5aebe0753e7aafe23ca07e050b9a9708c33ab26f
kemunto_97 <kemuntonyamweya@gmail.com>
2022-09-15 12:55:38 -0700

    cor1

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@
  * void prototypes (void)
  * int prototypes (int)
  */
-
+int _putchar(char c);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -15,8 +15,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-int _putchar(char);
-int _isupper(int c)
+int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 
