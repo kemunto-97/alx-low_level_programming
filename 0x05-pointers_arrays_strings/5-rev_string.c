2d1fe57b2fdeb69667ce6e3606c3004c0b43b076
kemunto_97 <kemuntonyamweya@gmail.com>
2022-09-19 09:23:56 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses string
+ * @s: string to be reversed
+ * Return: nothing
+ */
+
+void rev_string(char *s)
+{
+int i = 0;
+while (*(str + i) != '\0')
+{
+if (i % 2 == 0)
+{
+putchar(*(str + i));
+i++;
+}
+putchar(10);
+}
