9a3f39ddf2a6d63b11e2d7b7b2e4f9667f702373
kemunto_97 <kemuntonyamweya@gmail.com>
2022-09-28 05:16:03 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * find_root -  finds square root of n
+ * @n: n
+ * @root: test this root
+ * Return: natural square root
+ */
+
+int find_root(int n, int root)
+{
+if (root * root > n)
+return (-1);
+
+if (root * root == n)
+return (root)
+
+return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - find natural square root of n
+ * @n: integer
+ * Return: matural square root
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+return (find_root(n, 0));
+}
