82f4d2017b31f06eef894a4811e4959e67c76e25
kemunto_97 <kemuntonyamweya@gmail.com>
2022-10-04 01:01:56 -0700

    1-strdup.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,8 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int index;
 	if (size <= 0)
+	{
 		return (NULL);
-
+	}
 	array = malloc(sizeof(char) * size);
 	if (array == NULL)
 		return (NULL);
