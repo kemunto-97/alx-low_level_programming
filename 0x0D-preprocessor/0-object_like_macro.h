e234421fd98c51617f3dfdf6631c01985076a00d
kemunto_97 <kemuntonyamweya@gmail.com>
2022-10-12 04:30:16 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * File: 0-object_like_macro.h
+ *  Desc: Header file that defines a macro named
+ */
+
+#define SIZE 1024
+
+#endif
+
