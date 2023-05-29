22486295d9f01c5d72fa3525b33a323ec9a13351
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-05-29 19:04:21 +0300

    Create 1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+#include <stddef.h> /* for size_t */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+    size_t count = 0;
+    while (h != NULL)
+    {
+      count++;
+      h = h->next;
+    }
+  return count;
+}
+
+
+
+
