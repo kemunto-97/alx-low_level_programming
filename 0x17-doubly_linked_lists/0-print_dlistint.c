6a7c31007149c7627353924d799b339a9fde7a97
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-05-29 18:53:58 +0300

    Update 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -4,7 +4,6 @@
 size_t print_dlistint(const dlistint_t *h)
 {
     size_t node_count = 0; /* Variable to keep track of the number of nodes*/
-
     /* Traverse the list and print the elements*/
     while (h != NULL)
     {
