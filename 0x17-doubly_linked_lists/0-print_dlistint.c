006c373bf505dd1736d201c97d0f078ea7991723
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-05-29 12:17:21 +0300

    Create 0-print_dlistint.c

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "dlistint_t.h" // header
+
+size_t print_dlistint(const dlistint_t *h)
+{
+    size_t node_count = 0; // Variable to keep track of the number of nodes
+
+    // Traverse the list and print the elements
+    while (h != NULL)
+    {
+        printf("%d\n", h->n);
+        h = h->next;
+        node_count++;
+    }
+
+    return node_count;
+}
