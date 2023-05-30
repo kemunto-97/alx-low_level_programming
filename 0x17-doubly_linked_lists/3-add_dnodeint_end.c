9710bf0bd20c29e5b3eb6598b81a1e965d32f70e
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-05-30 15:57:13 +0300

    Update 3-add_dnodeint_end.c

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	new_node->n = n;
 	new_node->next = NULL;
 
-	if (*head == NULL) // If the list is empty
+	if (*head == NULL) /* If the list is empty */
 	{
 		new_node->prev = NULL;
 		*head = new_node;
