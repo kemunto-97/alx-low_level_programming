619ebc372694e074bfbd433b85a7c218c9210de5
Dianah Nyamweya <85859798+kemunto-97@users.noreply.github.com>
2023-06-05 16:26:55 +0300

    Update 8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+
 #include "lists.h"
 
 /**
@@ -9,29 +10,31 @@
  *
  * Return: 1 if the deletion is successful, -1 if it fails.
  */
+
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+
 {
-    dlistint_t *current;
-    unsigned int i;
 
-    if (*head == NULL)
-        return (-1);
+	dlistint_t *current;	
+	unsigned int i;
 
-    current = *head;
+	if (*head == NULL)
+		return (-1); 
 
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head != NULL)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
+	current = *head;
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head != NULL)
+			(*head)->prev = NULL;
+		free(current);
 
-    for (i = 0; current != NULL && i < index; i++)
+		return (1);
+	}
+
+	for (i = 0; current != NULL && i < index; i++)
         current = current->next;
 
-    if (current == NULL)
-        return (-1);
-
+	if (current == NULL)
+		return (-1); 
 }
